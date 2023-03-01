df0d1a2ccb3fead7b06f20c51d1c2d77577746a1
Alt B 2022 <massie.w@gmail.com>
2023-03-01 14:15:06 -0800

    Created get route in noteRoutes. Misc bug fixes.  Ensured proper connection of files and respective requirements/dependencies

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {readFromFile} = require('../../helpers/fsUtils');
 
 router.get('/notes', (req, res) => {
-
+readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 router.post('/notes', (req, res) => {
