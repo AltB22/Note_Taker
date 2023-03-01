b8fe97cb9fe31a77f2035dd1f8c27871846b160f
Alt B 2022 <massie.w@gmail.com>
2023-03-01 14:24:24 -0800

    Began work on post route in noteRoutes. Still needs refining as testing unsuccessful

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
-const {readFromFile} = require('../../helpers/fsUtils');
+const {readFromFile, writeToFile} = require('../../helpers/fsUtils');
 
 router.get('/notes', (req, res) => {
 readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 router.post('/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
 
-})
+  const note = writeToFile(req.body, res);
+  res.json(note);
+});
 
 
 module.exports = router
